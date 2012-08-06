a6253397366a160f3f562b692ce405c4f02b17fc
Phil <pnomolos@gmail.com>
2012-08-06 15:01:41 -0700

    Tweaking generate.php to work for li3

--- generate.php
+++ generate.php
@@ -15,17 +15,19 @@ require_once RUCKUSING_BASE . '/config/config.inc.php';
 require_once RUCKUSING_BASE  . '/lib/classes/util/class.Ruckusing_NamingUtil.php';
 require_once RUCKUSING_BASE  . '/lib/classes/util/class.Ruckusing_MigratorUtil.php';
 =======*/
-define('RUCKUSING_BASE', realpath(dirname(__FILE__)));
-require RUCKUSING_BASE . '/config/config.inc.php';
-require RUCKUSING_BASE . '/config/database.inc.php';
-require RUCKUSING_BASE . '/lib/classes/util/class.Ruckusing_Logger.php';
-require RUCKUSING_BASE . '/lib/classes/util/class.Ruckusing_NamingUtil.php';
-require RUCKUSING_BASE . '/lib/classes/util/class.Ruckusing_MigratorUtil.php';
-require RUCKUSING_BASE . '/lib/classes/class.Ruckusing_FrameworkRunner.php';
+if (!defined('RUCKUSING_BASE')) {
+	define('RUCKUSING_BASE', realpath(dirname(__FILE__)));
+}
+require_once RUCKUSING_BASE . '/config/config.inc.php';
+// require_once RUCKUSING_BASE . '/config/database.inc.php';
+require_once RUCKUSING_BASE . '/lib/classes/util/class.Ruckusing_Logger.php';
+require_once RUCKUSING_BASE . '/lib/classes/util/class.Ruckusing_NamingUtil.php';
+require_once RUCKUSING_BASE . '/lib/classes/util/class.Ruckusing_MigratorUtil.php';
+require_once RUCKUSING_BASE . '/lib/classes/class.Ruckusing_FrameworkRunner.php';
 //>>>>>>> upstream-branch
 
 $args = parse_args($argv);
-$framework = new Ruckusing_FrameworkRunner($ruckusing_db_config, null);
+$framework = new Ruckusing_FrameworkRunner($ruckusing_db_config, $argv);
 //input sanity check
 if(!is_array($args) || (is_array($args) && !array_key_exists('name', $args)) ) {
   print_help(true);
